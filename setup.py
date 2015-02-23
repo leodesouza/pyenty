b16cd0eea9eb3f4f8b05073ebd142c7abc6cf58e
leo.desouza@gmail.com <leo.desouza@gmail.com>
2015-02-23 17:51:51 -0300

    new release

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 import os
 from distutils.core import setup
 
-version = 'v0.1.3'
+version = '0.1.1'
 
 setup(
-    name="entmapper",
-    packages=['entmapper'],
+    name="pyenty",
+    packages=['pyenty'],
     version=version,
     keywords=["mongo", "mongodb", "entity", "motor", "tornado"],
     description="An simple object-document mapping for mongodb-motor and tornado applications.",
     author="Leonardo Souza",
     author_email="leo.desouza@gmail.com",
     url="https://github.com/leodesouza/entity",
-    download_url="https://github.com/leodesouza/entity/tree/v0.1.0-beta",
+    download_url="https://github.com/leodesouza/pyenty/tree/v0.1.1",
     classifiers=[
         "License :: OSI Approved :: Apache Software License",
     ],
